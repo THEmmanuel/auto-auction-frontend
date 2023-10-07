20599bfee60759202b85e619a65d2881c53c4609
Emmanuel <emmayodayo@gmail.com>
2023-10-08 00:32:20 +0100

    render (test) components

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import style from './NavBar.module.css';
+
+const NavBar = () => {
+	return (
+		<nav>
+			logo
+
+			<div>
+				<span>home</span>
+				<span>listings</span>
+				<span>history</span>
+			</div>
+		</nav>
+	)
+}
+
+export default NavBar;
