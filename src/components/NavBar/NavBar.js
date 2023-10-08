297e74aac76f6412b0af42b6e1e980cb1b86ca27
Emmanuel <emmayodayo@gmail.com>
2023-10-08 10:00:54 +0100

    add styling

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,10 +1,11 @@
 import React from "react";
 import style from './NavBar.module.css';
+import logo from '../../assets/carIcon.svg';
 
 const NavBar = () => {
 	return (
 		<nav>
-			logo
+			<img src={logo} alt="" />
 
 			<div>
 				<span>home</span>
