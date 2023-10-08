78746f5b80ce99e09671aff79b971c671d81ee81
Emmanuel <emmayodayo@gmail.com>
2023-10-08 15:03:14 +0100

    add car category content

--- src/data/carCategories.js
+++ src/data/carCategories.js
@@ -11,10 +11,70 @@ import classic from '../assets/classic.svg';
 
 
 const carCategories = [{
-	categoryImage: sedan,
-	categoryName: 'Sedan',
-	categoryDescription: 'Sleek and versatile, sedans offer a comfortable and efficient ride with spacious interiors, making them a popular choice for daily commuting.',
-	categoryColor: '#0C213A'
-}, ];
+		categoryImage: sedan,
+		categoryName: 'Sedans',
+		categoryDescription: 'Sleek and versatile, sedans offer a comfortable and efficient ride with spacious interiors, making them a popular choice for daily commuting.',
+		categoryColor: '#0C213A'
+	},
+
+	{
+		categoryImage: suv,
+		categoryName: 'SUVs',
+		categoryDescription: 'SUVs combine rugged capability with ample cargo space, making them ideal for adventurous trips and family journeys',
+		categoryColor: '#be5757'
+	},
+
+	{
+		categoryImage: coupe,
+		categoryName: 'Coupes',
+		categoryDescription: 'With their sporty and compact design, coupes are known for their style and performance, often appealing to drivers seeking a dynamic driving experience.',
+		categoryColor: '#6b8740'
+	},
+
+	{
+		categoryImage: hatchback,
+		categoryName: 'Hatchbacks',
+		categoryDescription: 'Hatchbacks provide the convenience of extra cargo room, making them practical for urban driving and road trips.',
+		categoryColor: '#4a4e69'
+	},
+
+	{
+		categoryImage: convertible,
+		categoryName: 'Convertibles',
+		categoryDescription: 'Convertibles offer an open-air driving experience, perfect for those who enjoy the wind in their hair and the sun on their face.',
+		categoryColor: '#372e4c'
+	},
+
+	{
+		categoryImage: wagon,
+		categoryName: 'Wagons',
+		categoryDescription: 'Wagons excel in carrying both passengers and cargo, making them a versatile choice for families and outdoor enthusiasts.',
+		categoryColor: '#72829e'
+	},
+
+	{
+		categoryImage: truck,
+		categoryName: 'Trucks',
+		categoryDescription: 'Known for their power and towing capacity, trucks are essential for heavy-duty tasks and off-road adventures.',
+		categoryColor: '#423A3A'
+	},
+
+	{
+		categoryImage: van,
+		categoryName: 'Vans/Minivans',
+		categoryDescription: 'Vans and minivans prioritize spaciousness and comfort, making them suitable for large families and group travel.',
+		categoryColor: '#b68119'
+	},
+
+	{
+		categoryImage: classic,
+		categoryName: 'Classics',
+		categoryDescription: 'Classic cars are timeless and iconic, often cherished by collectors and enthusiasts for their historical significance and design.',
+		categoryColor: '#6d2aaa'
+	}
+
+	,
+
+];
 
 export default carCategories;
