41916c72138dc342d4428893214fe2eb501f0caf
Emmanuel <emmayodayo@gmail.com>
2023-10-09 10:09:25 +0100

    set up routes

--- src/App.js
+++ src/App.js
@@ -3,14 +3,36 @@ import './App.css';
 
 import Home from './pages/Home/Home';
 import NavBar from './components/NavBar/NavBar';
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route
+} from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-		<NavBar/>
-		<Home/>
-    </div>
-  );
+	return (
+		<Router>
+			<div className="App">
+				<NavBar />
+				<Routes>
+					<Route
+						exact
+						path='/'
+						element=<Home />
+					>
+					</Route>
+
+					<Route
+						exact
+						path='/home'
+						element=<Home />
+					>
+					</Route>
+
+				</Routes>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
