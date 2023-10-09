0d67007181dee75d436a2ab9f32b49733027b3fd
Emmanuel <emmayodayo@gmail.com>
2023-10-09 10:14:24 +0100

    add pages

---
+++ src/pages/Post/Post.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './Post.module.css';
+
+const Post = props => {
+	return (
+		<section>
+			Post page here
+		</section>
+	)
+}
+
+export default Post;
