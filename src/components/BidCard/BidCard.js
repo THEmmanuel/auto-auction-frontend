4b2ea98788a2e14ccb3ea471015015236c64f8aa
Emmanuel <emmayodayo@gmail.com>
2023-10-11 09:34:13 +0100

    add components and structure

---
+++ src/components/BidCard/BidCard.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import style from './BidCard.module.css';
+
+const BidCard = () => {
+	return (
+		<div>
+			<div>
+				<div>
+					color
+				</div>
+
+				<span>
+					wallet address
+				</span>
+
+				Bid
+				<span>
+					30.8ETH
+				</span>
+
+				<span>
+					$35000
+				</span>
+
+			</div>
+
+		</div>
+	)
+}
+
+export default BidCard;
--- src/pages/Auction/Auction.js
+++ src/pages/Auction/Auction.js
@@ -1,10 +1,74 @@
 import React from 'react';
 import style from './Auction.module.css';
+import BidCard from '../../components/BidCard/BidCard';
+import TimeIcon from '../../assets/timerIcon.svg';
+import PrimaryButton from '../../components/PrimaryButton/PrimaryButton';
+
 
 const Auction = props => {
 	return (
-		<section>
-			Auction page here
+		<section className={style.AuctionWrapper}>
+			<div>
+				<div>
+					<span>Car name</span>
+					image component
+				</div>
+
+				<div>
+					<span>Highest bid</span>
+					<div>
+						<span>30.8ETH</span>
+						<span>Price in $</span>
+					</div>
+
+					<div>
+						<img src={TimeIcon} alt="" />
+						<span>Bid Time Left</span>
+						<span>Time</span>
+					</div>
+
+					<div>
+						<PrimaryButton
+							text='Place Bid'
+							width={150}
+						/>
+
+						<PrimaryButton
+							text='Contact Seller'
+							width={150}
+						/>
+					</div>
+				</div>
+
+				<div>
+					<span>10 bids</span>
+					<BidCard />
+					<BidCard />
+					<BidCard />
+					<BidCard />
+					<BidCard />
+					<BidCard />
+					<BidCard />
+					<BidCard />
+					<BidCard />
+				</div>
+			</div>
+
+			<div>
+				<div>
+					CarStats
+				</div>
+
+				<div>
+					<span>
+						About this car
+					</span>
+
+					<span>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea natus recusandae ipsum ut necessitatibus eum repellat dolor minima, nulla dolorum corrupti voluptatem. Accusamus commodi, placeat ea autem voluptatem ratione maiores!
+					</span>
+				</div>
+			</div>
 		</section>
 	)
 }
