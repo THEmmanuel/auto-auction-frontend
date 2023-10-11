907b197de8e67dddbd4ce73aceaf81b7770af125
Emmanuel <emmayodayo@gmail.com>
2023-10-11 23:46:31 +0100

    add styling

--- src/components/CarStats/CarStats.js
+++ src/components/CarStats/CarStats.js
@@ -49,7 +49,60 @@ const CarStats = props => {
 			</div>
 
 			<div>
+				<div className={style.CarStatInfoWrapper}>
+					<div className={style.CarStatInfo}>
+						<span className={style.CarStatText}>
+							Seller Wallet Address
+						</span>
+						<span className={style.CarStat}>
+							0xdB055877e6c13b6A6B25aBcAA29B393777dD0a73
+						</span>
+					</div>
 
+					<div className={style.CarStatInfo}>
+						<span className={style.CarStatText}>NFT contract address</span>
+						<span className={style.CarStat}>0xdB055877e6c13b6A6B25aBcAA29B393777dD0a73</span>
+					</div>
+
+					<div className={style.CarStatInfo}>
+						<span className={style.CarStatText}>Seller Location</span>
+						<span className={style.CarStat}>Redmond, WA. 22356</span>
+					</div>
+
+					<div className={style.CarStatInfo}>
+						<span className={style.CarStatText}>Exterior Colour</span>
+						<span className={style.CarStat}>
+							Nero DS/Rosso Corsa
+						</span>
+					</div>
+
+					<div className={style.CarStatInfo}>
+						<span className={style.CarStatText}>
+							Interior Colour
+						</span>
+						<span className={style.CarStat}>
+							Nero
+						</span>
+					</div>
+
+					<div className={style.CarStatInfo}>
+						<span className={style.CarStatText}>
+							Horsepower
+						</span>
+						<span className={style.CarStat}>
+							750hp
+						</span>
+					</div>
+
+					<div className={style.CarStatInfo}>
+						<span className={style.CarStatText}>
+							Seller Type
+						</span>
+						<span className={style.CarStat}>
+							Owner
+						</span>
+					</div>
+				</div>
 			</div>
 		</div>
 	)
