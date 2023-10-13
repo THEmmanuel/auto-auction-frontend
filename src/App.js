7a513487aeb035e3e895450d8f48f99d086d0951
Emmanuel <emmayodayo@gmail.com>
2023-10-13 15:36:12 +0100

    add rainbow kit imports and set up chains

--- src/App.js
+++ src/App.js
@@ -6,10 +6,8 @@ import Auction from './pages/Auction/Auction';
 import Listings from './pages/Listings/Listings';
 import Post from './pages/Post/Post';
 import History from './pages/History/History';
-import { WagmiConfig } from 'wagmi';
 
 import NavBar from './components/NavBar/NavBar';
-import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
 
 import {
 	BrowserRouter as Router,
@@ -17,11 +15,54 @@ import {
 	Route
 } from 'react-router-dom';
 
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+	getDefaultWallets,
+	RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import {
+	goerli,
+	mantleTestnet,
+	polygonMumbai,
+	polygonZkEvmTestnet,
+	scrollTestnet
+} from 'wagmi/chains';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, publicClient } = configureChains(
+	[
+		goerli,
+		mantleTestnet,
+		polygonMumbai,
+		polygonZkEvmTestnet,
+		scrollTestnet],
+	[
+		alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
+		publicProvider()
+	]
+);
+
+const { connectors } = getDefaultWallets({
+	appName: 'My RainbowKit App',
+	projectId: 'YOUR_PROJECT_ID',
+	chains
+});
+
+const wagmiConfig = createConfig({
+	autoConnect: true,
+	connectors,
+	publicClient
+})
+
+
+
 
 function App() {
 	return (
-		<WagmiConfig>
-			<RainbowKitProvider>
+		<WagmiConfig config={wagmiConfig}>
+			<RainbowKitProvider chains={chains}>
 				<Router>
 					<div className="App">
 						<NavBar />
