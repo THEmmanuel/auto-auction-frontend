849bfcaa54e45b45d5c7ef20e4da2820a3d51e8c
Emmanuel <emmayodayo@gmail.com>
2023-10-17 00:30:16 +0100

    set up file uploads

--- src/components/FileUpload/FileUpload.js
+++ src/components/FileUpload/FileUpload.js
@@ -2,19 +2,32 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import style from './FileUpload.module.css'
 import PrimaryButton from '../PrimaryButton/PrimaryButton';
+import { Web3Storage } from 'web3.storage'
 
 import porsche from '../../assets/testPorsche.svg';
 
 const FileUpload = (props) => {
 	const [selectedImage, setSelectedImage] = useState(null);
 	const [uploadStatus, setUploadStatus] = useState('');
+	const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDMwYmQ0ODE1M0ZBOTYxOGZmZTdGNjEzODk2ODJCZDc1YTY4NkFjNGIiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2OTc0NDkxMzU3NzQsIm5hbWUiOiJhdXRvLWF1Y3Rpb24ifQ.wIjf5oyG2GQ9llHl7I0Xdk6WxoFbFj87UelGEh-O74I';
+	const [files, setFiles] = useState([]);
 
 	const API_URL = 'http://localhost:8080'
 
 
 	// Create a FormData object to send the file
-	const handleImageUpload = (event) => {
+	const handleImageUpload = async (event) => {
+		// event.preventDefault()
+		const client = new Web3Storage({ token })
 
+		const cid = await client.put(files, {
+			onRootCidReady: localCid => {
+				console.log('uploading files' + localCid)
+			},
+			onStoredChunk: bytes => console.log('uplpad ' + bytes.toLocaleString() + 'to web3 storage')
+		})
+		console.log('stuff is now on web3 storage' + cid)
+		console.log(`https://dweb.link/ipfs/${cid}`)
 	}
 
 
@@ -28,13 +41,18 @@ const FileUpload = (props) => {
 			<div
 				className={style.CarImageWrapper}
 			>
-				<input type="file" onChange={handleImageUpload} className={style.ImageUpload} />
+				<input
+					type="file"
+					onChange={e => setFiles(e.target.files)}
+					className={style.ImageUpload} />
+
 				{uploadStatus === 'uploading' && <p>Uploading...</p>}
 				{uploadStatus === 'uploaded' && <p>Uploaded</p>}
 				{selectedImage && <img src={selectedImage} alt="Uploaded" className={style.CarImage} />}
 				<PrimaryButton
 					text="Submit"
 					width={200}
+					click={() => handleImageUpload()}
 				/>
 			</div>
 		</div>
